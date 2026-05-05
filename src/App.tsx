be2e6b17b329bb88d0d5326520c854a231e0f2cf
Rick <rick.buter@gmail.com>
2026-05-05 10:23:46 +0200

    chore: remove tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Icon from "./components/Icon";
 
 const App = () => {
   return (
-    <div className={"flex items-center justify-center min-h-screen"}>
-      <Icon className={"w-24 h-24 animate-bounce"} icon={"cow"} />
+    <div>
+      <Icon icon="cow" />
     </div>
   );
 };
